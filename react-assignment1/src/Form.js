9c870b0d7a2fcfd567153861e085244c79e63691
kmgarcia23 <kgarci83@calpoly.edu>
2021-04-04 19:18:05 -0700

    Finished Form functionality

--- react-assignment1/src/Form.js
+++ react-assignment1/src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Form()
+function Form(props)
 {
     const[person, setPerson] = useState(
         {
@@ -15,6 +15,11 @@ function Form()
             setPerson({name:person['name'], job:value});
         else
             setPerson({name:value, job:person['job']});
+    }
+
+    function submitForm(){
+        props.handleSubmit(person);
+        setPerson({name:'', job:''})
     }
 
     return(
@@ -33,6 +38,10 @@ function Form()
                 id = "job"
                 value = {person.job}
                 onChange = {handleChange} />
+            <input
+                type = "button"
+                value = "Submit"
+                onClick = {submitForm} />
         </form>
     )
 }
